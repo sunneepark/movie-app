fd426e3bbed1117f5c8e2dc050af03d7d28968d4
sunneepark <hl2ozs@naver.com>
2022-06-12 01:41:59 +0900

    feat: movie detail & casts
    - 한 개의 영화에 대한 자세한 정보 노출 페이지
    - 토글버튼으로 출연진 사진 노출

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,21 +9,35 @@ function LandingPage() {
 
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
-    useEffect(() => {
-      const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-
+    const fetchMovies = (endpoint) => {
       fetch(endpoint)
       .then(response => response.json())
       .then(response => {
         console.log(response)
-        setMovies(response.results)
+        setMovies([...Movies, ...response.results])
         setMainMovieImage(response.results[0])
+        setCurrentPage(response.page)
       })
+    }
+
+    useEffect(() => {
+      
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetchMovies(endpoint)
 
     }, [])
     
     
+
+    const loadMoreItems = () => {
+        
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint)
+
+    }
+
     return (
         <div style={{ width: '100%', margin: '0'}}>
             
@@ -44,6 +58,7 @@ function LandingPage() {
                     {Movies && Movies.map((movie, index) => (
                         <React.Fragment key={index}>
                              <GridCards
+                                landingPage
                                 image={movie.poster_path ? 
                                 `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
                                 movieId={movie.id}
@@ -56,7 +71,7 @@ function LandingPage() {
             </div>
             
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button>Load More</button> 
+                <button onClick={ loadMoreItems }>Load More</button> 
             </div>
         </div>
     )
